7b460341bea94ca9c4ed80a172524c539b5e081d
Yoav Ludmer <yoav.ludmer@gmail.com>
2011-11-13 20:32:13 +0200

    finished layout and routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Chap3::Application.routes.draw do
+  get "users/new"
+
+	 match '/signup',  :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   
-  match '/', :to => 'pages#home'
+  root :to => 'pages#home'
   
 end
 
